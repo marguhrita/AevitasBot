5934f22bc4164a9eb857d42e19f4b23b287079ce
marguhrita <s2157894@ed.ac.uk>
2022-01-03 16:34:40 +0000

    extra comments

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -5,24 +5,33 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
+    #kicks a member
     @commands.command(name="kick")
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member : discord.Member):
-       
         await ctx.guild.kick(member)
         await ctx.send(str(member) + " was kicked")
 
+    @kick.error
+    async def kickError(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Insufficient Permissions: {error.name}")
 
+
+    #bans a member
     @commands.command(name="ban")
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member):
 
         await ctx.guild.ban(member)
         await ctx.send(str(member) + " was banned")
-        
 
+    @ban.error
+    async def banError(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Insufficient Permissions: {error.name}")
 
+    #unbans a member
     @commands.command(name = "unban")
     @commands.has_permissions(ban_members = True)
     async def unban(self, ctx, id : int):
@@ -30,6 +39,10 @@ class Moderation(commands.Cog):
         await ctx.guild.unban(user)
         await ctx.send(f"succesfully unbanned {user}")
 
+    @unban.error
+    async def banError(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"Insufficient Permissions: {error.name}")
 
 def setup(client):
     client.add_cog(Moderation(client))
